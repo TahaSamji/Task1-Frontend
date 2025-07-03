5d64cdbac94e81bd9bbf4bebaf2758e219f64156
Taha Shamji <taha.shamji@vidizmo.com>
2025-07-03 16:33:40 +0500

    Initialized

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// templateUrl: './app.html',
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  template: '<button>',
   styleUrl: './app.css'
 })
 export class App {
