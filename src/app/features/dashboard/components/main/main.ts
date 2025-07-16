556ddb84674c6915244e839a30f44fed1373249f
TahaSamji <tahashamji45@gmail.com>
2025-07-17 03:08:23 +0500

    UI changes

--- src/app/features/dashboard/components/main/main.ts
+++ src/app/features/dashboard/components/main/main.ts
@@ -1,17 +1,11 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { UploadService } from '../../services/upload.service';
-import { CompletedStorageService } from '../../../../core/storage/local/complete.storage.service';
 import { UploadProgressComponent } from './progressbar/upload-progress.component';
 import { RouterModule } from '@angular/router';
 import { AppbarComponent } from '../../../../shared/components/appbar/appbar.component';
 import { CommonModule } from '@angular/common';
 import { VideoPlayerComponent } from '../videoplayer/video-player.component';
-import { EncodingTableComponent } from '../encoding/encoding.component';
 import { UserUploadsComponent } from '../useruploads/user-uploads.component';
-import { VideoRenditionSelectorComponent } from '../videovariantselector/video-rendition-selector.component';
 import { ResumableUploadsComponent } from './resumableuploads/resumable-uploads.component';
-import { AzureStorageService } from '../../../../core/storage/cloud/azure-storage.service';
-import { EncodingStateService } from '../../services/encoding-state.service';
 import { UploadHandlerService } from '../../services/upload-handler.service';
 
 @Component({
@@ -20,7 +14,7 @@ import { UploadHandlerService } from '../../services/upload-handler.service';
   templateUrl: './main_component.html',
   styleUrls: ['./main_component.css'],
   imports: [
-    RouterModule, AppbarComponent, CommonModule, VideoPlayerComponent, EncodingTableComponent, UserUploadsComponent, VideoRenditionSelectorComponent, ResumableUploadsComponent, UploadProgressComponent
+    RouterModule, AppbarComponent, CommonModule, VideoPlayerComponent, UserUploadsComponent, ResumableUploadsComponent, UploadProgressComponent
   ]
 })
 export class UploadComponent implements OnInit {
